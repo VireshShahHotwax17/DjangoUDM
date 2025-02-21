b983790b7266ed0744d2b4b5b5b8d433c584d878
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-21 20:26:49 +0530

    Created Uom and Uom Type entity

--- GeneralModels/models.py
+++ GeneralModels/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# ----------- Enumeration Entities-------------------
 class EnumerationType(models.Model):
     enumTypeId = models.CharField(primary_key = True, max_length = 100)
     description = models.CharField(max_length = 200)
 
+
     class Meta:
         db_table = 'EnumerationType'
+
 
 class Enumeration(models.Model):
     enumId = models.CharField(primary_key = True, max_length = 100)
@@ -18,6 +22,30 @@ class Enumeration(models.Model):
     sequence_num = models.DecimalField(max_digits=20, decimal_places=0, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
 
+
     class Meta:
         db_table = 'Enumeration'
-    
+
+
+# ------------- Uom Entities -------------------
+class UomType(models.Model):
+    uomTypeId = models.CharField(primary_key = True, max_length = 100)
+    parentTypeId = models.ForeignKey('self', on_delete = models.CASCADE)
+    hasTable = models.CharField(max_length = 1)
+    description = models.CharField(max_length = 50)
+
+
+    class Meta:
+        db_table = 'UomType'
+
+
+class Uom(models.Model):
+    uomId = models.CharField(primary_key = True, max_length = 100)
+    uomTypeId = models.ForeignKey(UomType, on_delete = models.CASCADE)
+    abbreviation = models.CharField(max_length = 10)
+    numericCode = models.IntegerField()
+    description = models.CharField(max_length = 100)
+
+
+    class Meta:
+        db_table = 'Uom'
