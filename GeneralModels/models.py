c8486d7b18f796f1541e6fde5354814c3a34841d
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-21 20:54:30 +0530

    Uploaded initial data

--- GeneralModels/models.py
+++ GeneralModels/models.py
@@ -14,12 +14,12 @@ class EnumerationType(models.Model):
 
 class Enumeration(models.Model):
     enumId = models.CharField(primary_key = True, max_length = 100)
-    enum_type_id = models.ForeignKey(
+    enumTypeId = models.ForeignKey(
         'EnumerationType', on_delete=models.SET_NULL, null=True, blank=True, db_column='ENUM_TYPE_ID'
     )
     parent_enum_id = models.CharField(max_length=40, null=True, blank=True)
-    enum_code = models.CharField(max_length=255, null=True, blank=True)
-    sequence_num = models.DecimalField(max_digits=20, decimal_places=0, null=True, blank=True)
+    enumCode = models.CharField(max_length=255, null=True, blank=True)
+    sequenceId = models.DecimalField(max_digits=20, decimal_places=0, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
 
 
